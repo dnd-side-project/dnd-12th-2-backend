0949136f49b32dfafcbad6f4c1bbd0c32c1570d0
5hseok <ohhs1010@gmail.com>
2025-02-07 02:25:17 +0900

    :sparkles: feat(user): @UserId 사용자 ID를 꺼내오는 로직을 @UserId 어노테이션을 통해 사용할 수 있게 추가

---
+++ src/main/java/ac/dnd/dodal/common/interceptor/UserIdArgumentResolver.java
@@ -0,0 +1,37 @@
+package ac.dnd.dodal.common.interceptor;
+
+import ac.dnd.dodal.annotation.UserId;
+import ac.dnd.dodal.common.exception.UnauthorizedException;
+import ac.dnd.dodal.config.security.enums.E_security_code;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+
+@Component
+@Slf4j
+public class UserIdArgumentResolver implements HandlerMethodArgumentResolver {
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        log.info("supportsParameter = {}", parameter.getParameterType().equals(Long.class) &&
+                parameter.hasParameterAnnotation(UserId.class));
+        return parameter.getParameterType().equals(Long.class) && parameter.hasParameterAnnotation(UserId.class);
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter,
+                                  ModelAndViewContainer mavContainer,
+                                  NativeWebRequest webRequest,
+                                  WebDataBinderFactory binderFactory) throws Exception {
+        final Object userIdObj = webRequest.getAttribute("USER_ID", NativeWebRequest.SCOPE_REQUEST);
+        log.info("resolveArgument = {}", userIdObj);
+        if (userIdObj == null) {
+            throw new UnauthorizedException(E_security_code.ACCESS_DENIED_ERROR);
+        }
+        return Long.valueOf(userIdObj.toString());
+    }
+}
