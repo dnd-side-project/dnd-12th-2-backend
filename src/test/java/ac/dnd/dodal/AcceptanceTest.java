c8042d533b44c478891dc718c1e262c25070a5e6
김혜주 <sueelly@naver.com>
2025-02-08 06:20:33 +0900

    feat(AcceptanceTest): Set use defined port (9999)

--- src/test/java/ac/dnd/dodal/AcceptanceTest.java
+++ src/test/java/ac/dnd/dodal/AcceptanceTest.java
@@ -1,13 +1,20 @@
 package ac.dnd.dodal;
 
+import io.restassured.RestAssured;
+
+import org.junit.jupiter.api.BeforeAll;
+
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles("test")
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public abstract class AcceptanceTest {
 
-    @LocalServerPort
-    protected int port;
+    protected static final int port = 9999;
+
+    @BeforeAll
+    public static void setUp() {
+        RestAssured.port = port;
+    }
 }
