1f9467156ed08960e6fcfd6b717d31fbaf3fa9dc
김혜주 <sueelly@naver.com>
2025-02-08 08:21:35 +0900

    refactor(GoalExceptionCode): Code size 3-3

--- src/main/java/ac/dnd/dodal/domain/goal/exception/GoalExceptionCode.java
+++ src/main/java/ac/dnd/dodal/domain/goal/exception/GoalExceptionCode.java
@@ -10,13 +10,13 @@ import lombok.RequiredArgsConstructor;
 @Getter
 public enum GoalExceptionCode implements ResultCode {
 
-    GOAL_TITLE_EXCEED_MAX_LENGTH("GOAL_001",
+    GOAL_TITLE_EXCEED_MAX_LENGTH("GOA_001",
             "Goal title is too long: Max length is " + GoalConstraint.MAX_GOAL_TITLE_LENGTH),
-    GOAL_TITLE_EMPTY("GOAL_002", "Goal title is empty"),
-    DELETED_GOAL("GOAL_003", "Wrong Access: Deleted goal"),
-    GOAL_NOT_FOUND("GOAL_004", "Goal not found"),
-    GOAL_ALREADY_ACHIEVED("GOAL_005", "Goal already achieved"),
-    GOAL_ALREADY_DELETED("GOAL_006", "Goal already deleted"),
+    GOAL_TITLE_EMPTY("GOA_002", "Goal title is empty"),
+    DELETED_GOAL("GOA_003", "Wrong Access: Deleted goal"),
+    GOAL_NOT_FOUND("GOA_004", "Goal not found"),
+    GOAL_ALREADY_ACHIEVED("GOA_005", "Goal already achieved"),
+    GOAL_ALREADY_DELETED("GOA_006", "Goal already deleted"),
     ;
 
     private final String code;
