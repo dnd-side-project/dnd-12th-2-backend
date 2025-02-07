ef2af4c751c8d40ec0cbc2d799720592eaafdbeb
김혜주 <sueelly@naver.com>
2025-02-08 07:39:12 +0900

    test(UIFixture): Create various header fixture

---
+++ src/test/java/ac/dnd/dodal/ui/fixture/UIFixture.java
@@ -0,0 +1,26 @@
+package ac.dnd.dodal.ui.fixture;
+
+import java.util.Map;
+
+public class UIFixture {
+
+    public static Map<String, Object> createAuthorizationHeader(String accessToken) {
+        return Map.of("Authorization", "Bearer " + accessToken);
+    }
+
+    public static Map<String, Object> createRefreshAuthorizationHeader(String refreshToken) {
+        return Map.of("refreshToken", "Bearer " + refreshToken);
+    }
+
+    public static Map<String, Object> createUnauthorizedHeader() {
+        return Map.of("Authorization", "Bearer " + "invalid-access-token");
+    }
+
+    public static Map<String, Object> createUnauthorizedRefreshHeader() {
+        return Map.of("refreshToken", "Bearer " + "invalid-refresh-token");
+    }
+
+    public static Map<String, Object> createEmptyHeader() {
+        return Map.of();
+    }
+}
