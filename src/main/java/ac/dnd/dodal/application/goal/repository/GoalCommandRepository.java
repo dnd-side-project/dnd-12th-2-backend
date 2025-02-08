764cb59561a4fface4c672d446066e79143f6f74
김혜주 <sueelly@naver.com>
2025-02-08 10:20:32 +0900

    feat(GoalCommandRepository): Add findById method

--- src/main/java/ac/dnd/dodal/application/goal/repository/GoalCommandRepository.java
+++ src/main/java/ac/dnd/dodal/application/goal/repository/GoalCommandRepository.java
@@ -1,5 +1,7 @@
 package ac.dnd.dodal.application.goal.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,6 @@ import ac.dnd.dodal.domain.goal.model.Goal;
 
 @Repository
 public interface GoalCommandRepository extends JpaRepository<Goal, Long> {
-}
 
+    Optional<Goal> findById(Long goalId);
+}
