9e29f8e83451c3de3da9680956de6bf474b8b60e
김혜주 <sueelly@naver.com>
2025-02-08 10:06:34 +0900

    test(GoalFixture): Implement default goal / achieved goal / deleted goal object

---
+++ src/test/java/ac/dnd/dodal/domain/goal/GoalFixture.java
@@ -0,0 +1,25 @@
+package ac.dnd.dodal.domain.goal;
+
+import java.time.LocalDateTime;
+
+import ac.dnd.dodal.domain.goal.model.Goal;
+
+public class GoalFixture {
+
+    private static final Long USER_ID = 1L;
+    private static final Long GOAL_ID = 1L;
+    private static final String TITLE = "title";
+
+    public static Goal goal() {
+        return new Goal(GOAL_ID, USER_ID, TITLE, false, null, null, null);
+    }
+
+    public static Goal achievedGoal() {
+        return new Goal(GOAL_ID, USER_ID, TITLE, true, null, null, null);
+    }
+
+    public static Goal deletedGoal() {
+        return new Goal(GOAL_ID, USER_ID, TITLE, false, LocalDateTime.now(), LocalDateTime.now(),
+                LocalDateTime.now());
+    }
+}
