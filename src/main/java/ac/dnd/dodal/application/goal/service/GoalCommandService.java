616d0a5b1be657a5d4feac9b7b82f2aaae247554
김혜주 <sueelly@naver.com>
2025-02-08 10:20:10 +0900

    feat(GoalCommandService): Implement logic

--- src/main/java/ac/dnd/dodal/application/goal/service/GoalCommandService.java
+++ src/main/java/ac/dnd/dodal/application/goal/service/GoalCommandService.java
@@ -1,28 +1,49 @@
 package ac.dnd.dodal.application.goal.service;
 
+import lombok.RequiredArgsConstructor;
+
 import org.springframework.stereotype.Service;
 
+import ac.dnd.dodal.common.exception.NotFoundException;
+import ac.dnd.dodal.domain.goal.model.Goal;
+import ac.dnd.dodal.domain.goal.exception.GoalExceptionCode;
 import ac.dnd.dodal.application.goal.dto.command.AchieveGoalCommand;
 import ac.dnd.dodal.application.goal.dto.command.CreateGoalCommand;
 import ac.dnd.dodal.application.goal.dto.command.DeleteGoalCommand;
 import ac.dnd.dodal.application.goal.usecase.CreateGoalUseCase;
 import ac.dnd.dodal.application.goal.usecase.AchieveGoalUseCase;
 import ac.dnd.dodal.application.goal.usecase.DeleteGoalUseCase;
+import ac.dnd.dodal.application.goal.repository.GoalCommandRepository;
 
 @Service
+@RequiredArgsConstructor
 public class GoalCommandService
         implements CreateGoalUseCase, AchieveGoalUseCase, DeleteGoalUseCase {
 
+    private final GoalCommandRepository goalCommandRepository;
+
     @Override
     public Long create(CreateGoalCommand command) {
-        return null;
+        Goal goal = CreateGoalCommand.toEntity(command);
+
+        return goalCommandRepository.save(goal).getGoalId();
     }
 
     @Override
     public void achieve(AchieveGoalCommand command) {
+        Goal goal = goalCommandRepository.findById(command.goalId())
+                .orElseThrow(() -> new NotFoundException(GoalExceptionCode.GOAL_NOT_FOUND));
+        goal.achieve(command.userId());
+
+        goalCommandRepository.save(goal);
     }
 
     @Override
     public void delete(DeleteGoalCommand command) {
+        Goal goal = goalCommandRepository.findById(command.goalId())
+                .orElseThrow(() -> new NotFoundException(GoalExceptionCode.GOAL_NOT_FOUND));
+        goal.delete(command.userId());
+
+        goalCommandRepository.save(goal);
     }
 }
