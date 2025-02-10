e6485134a013a06fcbe3bdf14c9c7a43cd0e4385
김혜주 <sueelly@naver.com>
2025-02-10 21:05:13 +0900

    rename(GoalConstraints): From GoalConstraint, it will store set of contraints

--- src/main/java/ac/dnd/dodal/domain/goal/exception/GoalExceptionCode.java
+++ src/main/java/ac/dnd/dodal/domain/goal/exception/GoalExceptionCode.java
@@ -1,7 +1,7 @@
 package ac.dnd.dodal.domain.goal.exception;
 
 import ac.dnd.dodal.common.enums.ResultCode;
-import ac.dnd.dodal.domain.goal.constraint.GoalConstraint;
+import ac.dnd.dodal.domain.goal.constraint.GoalConstraints;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -11,7 +11,7 @@ import lombok.RequiredArgsConstructor;
 public enum GoalExceptionCode implements ResultCode {
 
     GOAL_TITLE_EXCEED_MAX_LENGTH("GOA_001",
-            "Goal title is too long: Max length is " + GoalConstraint.MAX_GOAL_TITLE_LENGTH),
+            "Goal title is too long: Max length is " + GoalConstraints.MAX_GOAL_TITLE_LENGTH),
     GOAL_TITLE_EMPTY("GOA_002", "Goal title is empty"),
     DELETED_GOAL("GOA_003", "Wrong Access: Deleted goal"),
     GOAL_NOT_FOUND("GOA_004", "Goal not found"),
