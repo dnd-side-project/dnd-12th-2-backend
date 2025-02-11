ec1cd19d67919ef6228b9275b9863a9c5d917288
김혜주 <sueelly@naver.com>
2025-02-12 06:57:33 +0900

    refactor: Remove unused import statement

--- src/main/java/ac/dnd/dodal/ui/auth/response/JwtTokenDto.java
+++ src/main/java/ac/dnd/dodal/ui/auth/response/JwtTokenDto.java
@@ -2,8 +2,8 @@ package ac.dnd.dodal.ui.auth.response;
 
 import jakarta.validation.constraints.NotNull;
 
-
 public record JwtTokenDto(
+
         @NotNull(message = "AccessToken은 null이 될 수 없습니다.")
         String accessToken,
         @NotNull(message = "RefreshToken은 null이 될 수 없습니다.")
