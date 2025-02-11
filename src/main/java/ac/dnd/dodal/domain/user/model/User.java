8f6b55c48a120470e1698ececeec64069230b00b
5hseok <ohhs1010@gmail.com>
2025-02-12 01:50:42 +0900

    :sparkles: feat(Apple-login): 애플 로그인 v1 구현
    Related to: #9

--- src/main/java/ac/dnd/dodal/domain/user/model/User.java
+++ src/main/java/ac/dnd/dodal/domain/user/model/User.java
@@ -40,6 +40,9 @@ public class User extends BaseEntity {
     @Column(name="refresh_token")
     private String refreshToken;
 
+    @Column(name="apple_refresh_token")
+    private String appleRefreshToken;
+
     public User(String nickname, String profileImageUrl, String deviceToken, String email, UserRole role) {
         super(LocalDateTime.now(), LocalDateTime.now(), null);
 
@@ -59,6 +62,10 @@ public class User extends BaseEntity {
         if (nickname != null && !nickname.isEmpty()) {
             this.nickname = nickname;
         }
+    }
+
+    public void updateAppleRefreshToken(String appleRefreshToken) {
+        this.appleRefreshToken = appleRefreshToken;
     }
 
     public void updateUserProfileImageUrl(String profileImageUrl) {
