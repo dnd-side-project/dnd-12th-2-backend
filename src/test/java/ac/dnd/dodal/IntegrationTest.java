7ff74c6c7af20535d375fbed40654f36322e7001
김혜주 <sueelly@naver.com>
2025-02-12 02:38:38 +0900

    refactor(IntgrationTest): Add transactional, autoconfiguremockmvc annotation

--- src/test/java/ac/dnd/dodal/IntegrationTest.java
+++ src/test/java/ac/dnd/dodal/IntegrationTest.java
@@ -1,10 +1,16 @@
 package ac.dnd.dodal;
 
-import org.springframework.boot.test.context.SpringBootTest;
+import jakarta.transaction.Transactional;
+
 import org.springframework.test.context.ActiveProfiles;
 
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+
 @ActiveProfiles("test")
+@Transactional
 @SpringBootTest
+@AutoConfigureMockMvc
 public abstract class IntegrationTest {
 
     // integration test에서 사용할 것들 모아놓기
