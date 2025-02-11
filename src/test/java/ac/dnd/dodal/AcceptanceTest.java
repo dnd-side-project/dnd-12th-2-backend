7b270cc71f74485080fe42a09e830471fee7fee3
김혜주 <sueelly@naver.com>
2025-02-11 22:03:22 +0900

    feat(AcceptanceTest): Add Transactional to reset after each test

--- src/test/java/ac/dnd/dodal/AcceptanceTest.java
+++ src/test/java/ac/dnd/dodal/AcceptanceTest.java
@@ -2,14 +2,18 @@ package ac.dnd.dodal;
 
 import java.util.Map;
 
+import jakarta.transaction.Transactional;
+
 import io.restassured.RestAssured;
 
 import org.junit.jupiter.api.BeforeAll;
 
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+
 import ac.dnd.dodal.ui.fixture.UIFixture;
 
+@Transactional
 @ActiveProfiles("test")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public abstract class AcceptanceTest {
