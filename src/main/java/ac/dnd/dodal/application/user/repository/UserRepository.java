c0dc07a5b46a88ceb437e4fe0dbe65b493a2ea32
5hseok <ohhs1010@gmail.com>
2025-02-12 14:12:23 +0900

    :art: chore(userRepository): 코드 conventions

--- src/main/java/ac/dnd/dodal/application/user/repository/UserRepository.java
+++ src/main/java/ac/dnd/dodal/application/user/repository/UserRepository.java
@@ -11,21 +11,23 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    @Query("UPDATE User u SET u.refreshToken = :refreshToken WHERE u.id = :id")
-    void updateRefreshToken(Long id, String refreshToken);
+  @Query("UPDATE User u SET u.refreshToken = :refreshToken WHERE u.id = :id")
+  void updateRefreshToken(Long id, String refreshToken);
 
-    @Query("SELECT u FROM User u WHERE u.email = :email")
-    Optional<User> findByEmail(String email);
+  @Query("SELECT u FROM User u WHERE u.email = :email")
+  Optional<User> findByEmail(String email);
 
-    @Query("SELECT u FROM User u WHERE u.id = :id AND u.refreshToken IS NOT NULL")
-    Optional<User> findByIdAndRefreshTokenNotNull(Long id);
+  @Query("SELECT u FROM User u WHERE u.id = :id AND u.refreshToken IS NOT NULL")
+  Optional<User> findByIdAndRefreshTokenNotNull(Long id);
 
   @Query("SELECT u FROM User u WHERE u.email = :email AND u.role = :role")
   User findByEmailAndRole(String email, UserRole role);
 
-  @Query("SELECT CASE WHEN COUNT(u) > 0 THEN TRUE ELSE FALSE END FROM User u WHERE u.email = :email")
+  @Query(
+      "SELECT CASE WHEN COUNT(u) > 0 THEN TRUE ELSE FALSE END FROM User u WHERE u.email = :email")
   boolean existsByEmail(String email);
 
-  @Query("SELECT CASE WHEN COUNT(u) > 0 THEN TRUE ELSE FALSE END FROM User u WHERE u.nickname = :nickname")
+  @Query(
+      "SELECT CASE WHEN COUNT(u) > 0 THEN TRUE ELSE FALSE END FROM User u WHERE u.nickname = :nickname")
   boolean existsByNickname(String nickname);
 }
