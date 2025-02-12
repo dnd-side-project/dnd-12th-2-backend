53b88e7cf9fafcb95d700fd0250714f55a1951c4
김혜주 <sueelly@naver.com>
2025-02-13 03:11:36 +0900

    feat(Goal/Plan/PlanHistoryService): Basic services commonly have save, saveAndFlush, findById

---
+++ src/main/java/ac/dnd/dodal/application/plan_history/service/PlanHistoryService.java
@@ -0,0 +1,41 @@
+package ac.dnd.dodal.application.plan_history.service;
+
+import java.util.Optional;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.stereotype.Service;
+
+import ac.dnd.dodal.common.exception.NotFoundException;
+import ac.dnd.dodal.domain.plan_history.exception.PlanHistoryExceptionCode;
+import ac.dnd.dodal.domain.plan_history.model.PlanHistory;
+import ac.dnd.dodal.application.plan_history.repository.PlanHistoryRepository;
+
+@Service
+@RequiredArgsConstructor
+public class PlanHistoryService {
+
+    private final PlanHistoryRepository planHistoryRepository;
+
+    public PlanHistory save(PlanHistory planHistory) {
+        return planHistoryRepository.save(planHistory);
+    }
+
+    public PlanHistory saveAndFlush(PlanHistory planHistory) {
+        return planHistoryRepository.saveAndFlush(planHistory);
+    }
+
+    public Optional<PlanHistory> findById(Long planHistoryId) {
+        return planHistoryRepository.findById(planHistoryId);
+    }
+
+    public PlanHistory findByIdOrThrow(Long planHistoryId) {
+        return planHistoryRepository.findById(planHistoryId).orElseThrow(
+                () -> new NotFoundException(PlanHistoryExceptionCode.PLAN_HISTORY_NOT_FOUND));
+    }
+
+    public void delete(PlanHistory planHistory) {
+        planHistory.delete();
+        planHistoryRepository.save(planHistory);
+    }
+}
