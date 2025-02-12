98c9a74de62be3682e303d9563768ecec79e13db
5hseok <ohhs1010@gmail.com>
2025-02-12 14:44:55 +0900

    :recycle: refactor(ui package 분리): ui package를 auth와 user로 나눔에 따라 생긴 변경사항

--- src/main/java/ac/dnd/dodal/application/user/usecase/UserQueryUseCase.java
+++ src/main/java/ac/dnd/dodal/application/user/usecase/UserQueryUseCase.java
@@ -7,16 +7,11 @@ import java.util.Optional;
 
 public interface UserQueryUseCase {
 
-    User findByEmailAndRole(String email, UserRole role);
-
-    Optional<User> findByEmail(String email);
-
-    void checkDuplicatedEmail(String email);
-
-    User findByIdAndRole(Long id, UserRole userRole);
-  void checkDuplicatedEmail(String email);
+  User findByEmailAndRole(String email, UserRole role);
 
   Optional<User> findByEmail(String email);
+
+  void checkDuplicatedEmail(String email);
 
   User findByIdAndRole(Long id, UserRole userRole);
 }
