042fb737fae541eb06109921b8fd25aed12fe6a5
김혜주 <sueelly@naver.com>
2025-02-13 03:21:06 +0900

    refactor(AchieveGoalAcceptanceTest): Applying sample data

--- src/test/java/ac/dnd/dodal/acceptance/goal/AchieveGoalAcceptanceTest.java
+++ src/test/java/ac/dnd/dodal/acceptance/goal/AchieveGoalAcceptanceTest.java
@@ -18,7 +18,7 @@ import ac.dnd.dodal.domain.goal.exception.GoalExceptionCode;
 
 public class AchieveGoalAcceptanceTest extends AcceptanceTest {
 
-    private Long unachievedGoalId = 4L;
+    private Long achievedGoalId = 3L;
     private Long deletedGoalId = 2L;
     private Long goalId = 1L;
 
@@ -26,7 +26,7 @@ public class AchieveGoalAcceptanceTest extends AcceptanceTest {
     @DisplayName("Achieve Goal Test")
     public void achieve_goal() {
         // when
-        Response response = GoalSteps.achieveGoal(authorizationHeader, unachievedGoalId);
+        Response response = GoalSteps.achieveGoal(authorizationHeader, goalId);
         ApiResponse<Long> apiResponse = response.as(new TypeRef<ApiResponse<Long>>() {});
 
         // then 200
@@ -38,24 +38,6 @@ public class AchieveGoalAcceptanceTest extends AcceptanceTest {
         // data does not exist
         assertThat(response.getBody().jsonPath().getMap("data")).isNull();
     }
-
-    // Todo: 인증이 추가된 이후 테스트 추가
-    // @Test
-    // @DisplayName("Achieve Goal Test with Unauthorized User")
-    // public void achieve_goal_with_unauthorized_user() {
-    //     // when
-    //     Response response = GoalSteps.achieveGoal(unauthorizedAuthorizationHeader, unachievedGoalId);
-    //     ApiResponse<Long> apiResponse = response.as(new TypeRef<ApiResponse<Long>>() {});
-
-    //     // then 401
-    //     assertThat(response.statusCode()).isEqualTo(HttpStatus.UNAUTHORIZED.value());
-    //     // COM003
-    //     assertThat(apiResponse.code()).isEqualTo(CommonResultCode.UNAUTHORIZED.getCode());
-    //     // Unauthorized
-    //     assertThat(apiResponse.message()).isEqualTo(CommonResultCode.UNAUTHORIZED.getMessage());
-    //     // data does not exist
-    //     assertThat(response.getBody().jsonPath().getMap("data")).isNull();
-    // }
 
     @Test
     @DisplayName("Achieve Goal Test with Deleted Goal")
@@ -78,7 +60,7 @@ public class AchieveGoalAcceptanceTest extends AcceptanceTest {
     @DisplayName("Achieve Goal Test with Already Achieved Goal")
     public void achieve_goal_with_already_achieved_goal() {
         // when
-        Response response = GoalSteps.achieveGoal(authorizationHeader, goalId);
+        Response response = GoalSteps.achieveGoal(authorizationHeader, achievedGoalId);
         ApiResponse<Long> apiResponse = response.as(new TypeRef<ApiResponse<Long>>() {});
 
         // then 403
