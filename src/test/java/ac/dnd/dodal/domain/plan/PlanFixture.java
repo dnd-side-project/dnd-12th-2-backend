3331ebb6a288aa87cb0e6c7c57710967e60cebfb
김혜주 <sueelly@naver.com>
2025-02-13 14:40:56 +0900

    refactor(PlanFixture): Apply completedDate field

--- src/test/java/ac/dnd/dodal/domain/plan/PlanFixture.java
+++ src/test/java/ac/dnd/dodal/domain/plan/PlanFixture.java
@@ -21,95 +21,95 @@ public class PlanFixture {
     public static Plan plan() {
         return new Plan(PLAN_ID, GOAL, HISTORY,
             "title", PlanStatus.NONE, null,
-            LocalDateTime.now(), LocalDateTime.now().plusDays(1),
+            LocalDateTime.now(), LocalDateTime.now().plusDays(1), null,
             LocalDateTime.now(), LocalDateTime.now(), null);
     }
 
     public static Plan plan(String title, PlanStatus status, String guide,
-            LocalDateTime startDate, LocalDateTime endDate) {
+            LocalDateTime startDate, LocalDateTime endDate, LocalDateTime completedDate) {
         return new Plan(PLAN_ID, GOAL, HISTORY, title, status, guide, startDate, endDate,
-            LocalDateTime.now(), LocalDateTime.now(), null);
+            completedDate, LocalDateTime.now(), LocalDateTime.now(), null);
     }
 
     public static Plan deletedPlan() {
         return new Plan(PLAN_ID, GOAL, HISTORY,
             "title", PlanStatus.NONE, null,
-            LocalDateTime.now(), LocalDateTime.now().plusDays(1),
+            LocalDateTime.now(), LocalDateTime.now().plusDays(1), null,
             LocalDateTime.now(), LocalDateTime.now(), LocalDateTime.now());
     }
 
     public static Plan deletedPlan(String title, PlanStatus status, String guide,
-            LocalDateTime startDate, LocalDateTime endDate) {
+            LocalDateTime startDate, LocalDateTime endDate, LocalDateTime completedDate) {
         return new Plan(PLAN_ID, GOAL, HISTORY,
-            title, status, guide, startDate, endDate,
+            title, status, guide, startDate, endDate, completedDate,
             LocalDateTime.now(), LocalDateTime.now(), LocalDateTime.now());
     }
 
     public static Plan successPlan() {
         return new Plan(PLAN_ID, GOAL, HISTORY,
             "title", PlanStatus.SUCCESS, "guide",
-            LocalDateTime.now(), LocalDateTime.now().plusDays(1),
+            LocalDateTime.now(), LocalDateTime.now().plusDays(1), LocalDateTime.now().plusDays(1),
             LocalDateTime.now(), LocalDateTime.now(), null);
     }
 
     public static Plan successPlan(String title, String guide,
-            LocalDateTime startDate, LocalDateTime endDate) {
+            LocalDateTime startDate, LocalDateTime endDate, LocalDateTime completedDate) {
         return new Plan(PLAN_ID, GOAL, HISTORY,
-            title, PlanStatus.SUCCESS, guide, startDate, endDate,
+            title, PlanStatus.SUCCESS, guide, startDate, endDate, completedDate,
             LocalDateTime.now(), LocalDateTime.now(), null);
     }
 
     public static Plan failurePlan() {
         return new Plan(PLAN_ID, GOAL, HISTORY, "title", PlanStatus.FAILURE, "guide",
-            LocalDateTime.now(), LocalDateTime.now().plusDays(1), LocalDateTime.now(),
-            LocalDateTime.now(), null);
+                LocalDateTime.now(), LocalDateTime.now().plusDays(1), LocalDateTime.now().plusDays(1),
+                LocalDateTime.now(), LocalDateTime.now(), null);
     }
 
     public static Plan failurePlan(String title, String guide,
-            LocalDateTime startDate, LocalDateTime endDate) {
+            LocalDateTime startDate, LocalDateTime endDate, LocalDateTime completedDate) {
         return new Plan(PLAN_ID, GOAL, HISTORY,
-            title, PlanStatus.FAILURE, guide, startDate, endDate,
+            title, PlanStatus.FAILURE, guide, startDate, endDate, completedDate,
             LocalDateTime.now(), LocalDateTime.now(), null);
     }
 
     public static Plan tommorowStartPlan() {
         return new Plan(PLAN_ID, GOAL, HISTORY, "title", PlanStatus.NONE, null,
-            LocalDateTime.now().plusDays(1), LocalDateTime.now().plusDays(2),
+            LocalDateTime.now().plusDays(1), LocalDateTime.now().plusDays(2), null,
             LocalDateTime.now(), LocalDateTime.now(), null);
     }
 
     public static List<Plan> monthlyPlan() {
         return Arrays.asList(
             successPlan("success plan", "guide",
-                LocalDateTime.now().minusDays(10), LocalDateTime.now().minusDays(9)),
+                LocalDateTime.now().minusDays(10), LocalDateTime.now().minusDays(9), LocalDateTime.now().minusDays(9)),
             successPlan("success plan", "guide", LocalDateTime.now().minusDays(9),
-                LocalDateTime.now().minusDays(8)),
+                LocalDateTime.now().minusDays(8), LocalDateTime.now().minusDays(8)),
             successPlan("success plan", "guide", LocalDateTime.now().minusDays(8),
-                LocalDateTime.now().minusDays(7)),
+                LocalDateTime.now().minusDays(7), LocalDateTime.now().minusDays(7)),
             successPlan("success plan", "guide", LocalDateTime.now().minusDays(7),
-                LocalDateTime.now().minusDays(6)),
+                LocalDateTime.now().minusDays(6), LocalDateTime.now().minusDays(6)),
             successPlan("success plan", "guide", LocalDateTime.now().minusDays(6),
-                LocalDateTime.now().minusDays(5)),
+                LocalDateTime.now().minusDays(5), LocalDateTime.now().minusDays(5)),
             successPlan("success plan", "guide", LocalDateTime.now().minusDays(5),
-                LocalDateTime.now().minusDays(4)),
+                LocalDateTime.now().minusDays(4), LocalDateTime.now().minusDays(4)),
             successPlan("success plan", "guide", LocalDateTime.now().minusDays(4),
-                LocalDateTime.now().minusDays(3)),
+                LocalDateTime.now().minusDays(3), LocalDateTime.now().minusDays(3)),
             successPlan("success plan", "guide", LocalDateTime.now().minusDays(3),
-                LocalDateTime.now().minusDays(2)),
+                LocalDateTime.now().minusDays(2), LocalDateTime.now().minusDays(2)),
             successPlan("success plan", "guide", LocalDateTime.now().minusDays(2),
-                LocalDateTime.now().minusDays(1)),
+                LocalDateTime.now().minusDays(1), LocalDateTime.now().minusDays(1)),
             successPlan("success plan", "guide", LocalDateTime.now().minusDays(1),
-                LocalDateTime.now()),
+                LocalDateTime.now(), LocalDateTime.now()),
             plan("plan", PlanStatus.NONE, null,
-                LocalDateTime.now(), LocalDateTime.now().plusDays(1)),
+                LocalDateTime.now(), LocalDateTime.now().plusDays(1), null),
             plan("plan", PlanStatus.NONE, null,
-                LocalDateTime.now().plusDays(1), LocalDateTime.now().plusDays(2)),
+                LocalDateTime.now().plusDays(1), LocalDateTime.now().plusDays(2), null),
             plan("plan", PlanStatus.NONE, null,
-                LocalDateTime.now().plusDays(2), LocalDateTime.now().plusDays(3)),
+                LocalDateTime.now().plusDays(2), LocalDateTime.now().plusDays(3), null),
             plan("plan", PlanStatus.NONE, null,
-                LocalDateTime.now().plusDays(3), LocalDateTime.now().plusDays(4)),
+                LocalDateTime.now().plusDays(3), LocalDateTime.now().plusDays(4), null),
             plan("plan", PlanStatus.NONE, null,
-                LocalDateTime.now().plusDays(4), LocalDateTime.now().plusDays(5)));
+                LocalDateTime.now().plusDays(4), LocalDateTime.now().plusDays(5), null));
     }
 
     public static List<Plan> plans() {
