f8e49c615fa20a9717dc2b854dbdc9a32cbe74f8
김혜주 <sueelly@naver.com>
2025-02-13 13:11:49 +0900

    feat(PlanStatus): Add value for parsing form url
    Related to #37

--- src/main/java/ac/dnd/dodal/domain/plan/enums/PlanStatus.java
+++ src/main/java/ac/dnd/dodal/domain/plan/enums/PlanStatus.java
@@ -1,9 +1,28 @@
 package ac.dnd.dodal.domain.plan.enums;
 
+import java.util.Arrays;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import ac.dnd.dodal.common.exception.ForbiddenException;
+import ac.dnd.dodal.domain.plan.exception.PlanExceptionCode;
+
+@Getter
+@RequiredArgsConstructor
 public enum PlanStatus {
     
-    NONE,
-    SUCCESS,
-    FAILURE,
+    NONE("none"),
+    SUCCESS("success"),
+    FAILURE("failure"),
     ;
+
+    private final String value;
+
+    public static PlanStatus of(String value) {
+        return Arrays.stream(PlanStatus.values())
+            .filter(status -> status.getValue().equals(value))
+            .findFirst()
+            .orElseThrow(() -> new ForbiddenException(PlanExceptionCode.INVALID_PLAN_STATUS));
+    }
 }
