7f2a5f6b5d1aa6c97c5a90c0456ed6ea96111f7c
김혜주 <sueelly@naver.com>
2025-02-14 23:12:31 +0900

    feat(HistoryStatisticsEventListener): Update historyStatistics when plan completed

---
+++ src/main/java/ac/dnd/dodal/application/plan_history/service/HistoryStatisticsEventListenter.java
@@ -0,0 +1,25 @@
+package ac.dnd.dodal.application.plan_history.service;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+import ac.dnd.dodal.domain.plan_history.model.HistoryStatistics;
+import ac.dnd.dodal.domain.plan.event.PlanCompletedEvent;
+
+@Component
+@RequiredArgsConstructor
+public class HistoryStatisticsEventListenter {
+
+    private final HistoryStatisticsService historyStatisticsService;
+
+    @EventListener
+    public void handlePlanCompletedEvent(PlanCompletedEvent event) {
+        HistoryStatistics historyStatistics =
+            historyStatisticsService.findByIdOrThrow(event.getPlanId());
+
+        historyStatistics.incrementCount(event.getStatus());
+        historyStatisticsService.save(historyStatistics);
+    }
+}
