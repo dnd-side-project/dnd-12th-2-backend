efd7e4bd879c489f14c95de4af4d5cfa01072ae6
김혜주 <sueelly@naver.com>
2025-02-14 23:13:55 +0900

    feat(GoalCommandService): Publish event when create goal

--- src/main/java/ac/dnd/dodal/application/goal/service/GoalCommandService.java
+++ src/main/java/ac/dnd/dodal/application/goal/service/GoalCommandService.java
@@ -4,8 +4,10 @@ import lombok.RequiredArgsConstructor;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.context.ApplicationEventPublisher;
 
 import ac.dnd.dodal.domain.goal.model.Goal;
+import ac.dnd.dodal.domain.goal.event.GoalCreatedEvent;
 import ac.dnd.dodal.application.goal.dto.command.*;
 import ac.dnd.dodal.application.goal.usecase.CreateGoalUseCase;
 import ac.dnd.dodal.application.goal.usecase.AchieveGoalUseCase;
@@ -19,10 +21,13 @@ public class GoalCommandService
 
     private final GoalService goalService;
 
+    private final ApplicationEventPublisher eventPublisher;
+
     @Override
     public Long create(CreateGoalCommand command) {
         Goal goal = CreateGoalCommand.toEntity(command);
 
+        eventPublisher.publishEvent(new GoalCreatedEvent(goal.getGoalId()));
         return goalService.save(goal).getGoalId();
     }
 
