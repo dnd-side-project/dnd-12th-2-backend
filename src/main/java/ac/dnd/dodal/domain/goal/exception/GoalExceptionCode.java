1879cf6c02351230a13b32a3b207d800b9181711
김혜주 <sueelly@naver.com>
2025-02-15 00:57:01 +0900

    refactor(ExceptionCode): Remove _ from code

--- src/main/java/ac/dnd/dodal/domain/goal/exception/GoalExceptionCode.java
+++ src/main/java/ac/dnd/dodal/domain/goal/exception/GoalExceptionCode.java
@@ -10,15 +10,15 @@ import lombok.RequiredArgsConstructor;
 @Getter
 public enum GoalExceptionCode implements ResultCode {
 
-    GOAL_TITLE_EXCEED_MAX_LENGTH("GOA_001",
+    GOAL_TITLE_EXCEED_MAX_LENGTH("GOA001",
             "Goal title is too long: Max length is " + GoalConstraints.MAX_GOAL_TITLE_LENGTH),
-    GOAL_TITLE_EMPTY("GOA_002", "Goal title is empty"),
-    DELETED_GOAL("GOA_003", "Wrong Access: Deleted goal"),
-    GOAL_NOT_FOUND("GOA_004", "Goal not found"),
-    GOAL_ALREADY_ACHIEVED("GOA_005", "Goal already achieved"),
-    GOAL_ALREADY_DELETED("GOA_006", "Goal already deleted"),
-    ACHIEVED_GOAL("GOA_007", "Wrong Access: Achieved goal"),
-    GOAL_STATISTICS_NOT_FOUND("GOA_008", "Goal statistics not found"),
+    GOAL_TITLE_EMPTY("GOA002", "Goal title is empty"),
+    DELETED_GOAL("GOA003", "Wrong Access: Deleted goal"),
+    GOAL_NOT_FOUND("GOA004", "Goal not found"),
+    GOAL_ALREADY_ACHIEVED("GOA005", "Goal already achieved"),
+    GOAL_ALREADY_DELETED("GOA006", "Goal already deleted"),
+    ACHIEVED_GOAL("GOA007", "Wrong Access: Achieved goal"),
+    GOAL_STATISTICS_NOT_FOUND("GOA008", "Goal statistics not found"),
     ;
 
     private final String code;
