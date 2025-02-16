826b0711ccf212ebca589abf5a911350a02654eb
김혜주 <sueelly@naver.com>
2025-02-16 16:34:05 +0900

    feat(GetGoalStatitsticsAcceptanceTest): Implement test of get weekly achievement rate of goal

---
+++ src/test/java/ac/dnd/dodal/acceptance/goal/steps/GoalStatisticsSteps.java
@@ -0,0 +1,29 @@
+package ac.dnd.dodal.acceptance.goal.steps;
+
+import static io.restassured.RestAssured.given;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Map;
+
+import io.restassured.response.Response;
+
+public class GoalStatisticsSteps {
+    
+    private static final String BASE_URL = "/api/goals/{goalId}/statistics";
+
+    public static Response getWeeklyAchievementRateOfGoal(
+        Map<String, Object> header, Long goalId, LocalDate date) {
+        String url = BASE_URL.replace("{goalId}", goalId.toString());
+        String dateString = date.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+
+        return given().log().all()
+                .headers(header)
+                .param("date", dateString)
+            .when()
+                .get(url)
+            .then().log().all()
+                .extract()
+            .response();
+    }
+}
