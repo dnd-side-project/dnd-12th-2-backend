08c48153b642842bc889bec207b9980caa054568
김혜주 <sueelly@naver.com>
2025-02-17 10:50:42 +0900

    refactor(IntegrationTest): Move all autowired from classes which extends IntegrationTest

--- src/test/java/ac/dnd/dodal/IntegrationTest.java
+++ src/test/java/ac/dnd/dodal/IntegrationTest.java
@@ -2,10 +2,24 @@ package ac.dnd.dodal;
 
 import jakarta.transaction.Transactional;
 
-import org.springframework.test.context.ActiveProfiles;
+import org.jasypt.encryption.StringEncryptor;
 
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+import ac.dnd.dodal.core.security.util.JwtUtil;
+import ac.dnd.dodal.application.goal.service.GoalStatisticsService;
+import ac.dnd.dodal.application.goal.service.GoalService;
+import ac.dnd.dodal.application.goal.service.GoalStatisticsEventListener;
+import ac.dnd.dodal.application.user.repository.UserRepository;
+import ac.dnd.dodal.application.goal.repository.GoalRepository;
+import ac.dnd.dodal.application.goal.repository.GoalStatisticsRepository;
+import ac.dnd.dodal.application.plan.repository.PlanRepository;
 
 @ActiveProfiles("test")
 @Transactional
@@ -13,5 +27,28 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 @AutoConfigureMockMvc
 public abstract class IntegrationTest {
 
-    // integration test에서 사용할 것들 모아놓기
+    @Autowired
+    protected MockMvc mockMvc;
+    @Autowired
+    protected ApplicationEventPublisher eventPublisher;
+    @Autowired
+    protected GoalStatisticsEventListener goalStatisticsEventListener;
+    @Autowired
+    protected GoalStatisticsService goalStatisticsService;
+    @Autowired
+    protected GoalService goalService;
+    @Autowired
+    protected GoalRepository goalRepository;
+    @Autowired
+    protected GoalStatisticsRepository goalStatisticsRepository;
+    @Autowired
+    protected PlanRepository planRepository;
+    @Autowired
+    protected UserRepository userRepository;
+    @Autowired
+    protected JwtUtil jwtUtil;
+
+    @Autowired
+    @Qualifier("jasyptStringEncryptor")
+    protected StringEncryptor stringEncryptor;
 }
--- src/test/java/ac/dnd/dodal/config/JasyptConfigTest.java
+++ src/test/java/ac/dnd/dodal/config/JasyptConfigTest.java
@@ -4,18 +4,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-import org.jasypt.encryption.StringEncryptor;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-
 import ac.dnd.dodal.IntegrationTest;
 
 public class JasyptConfigTest extends IntegrationTest {
-
-    @Autowired
-    @Qualifier("jasyptStringEncryptor")
-    private StringEncryptor stringEncryptor;
 
     @Test
     public void testEncrypt() {
