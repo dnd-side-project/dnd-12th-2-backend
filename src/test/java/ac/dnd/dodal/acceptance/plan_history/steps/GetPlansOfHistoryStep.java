da0f3207a05d66a4d2d069bef82312edef28b180
김혜주 <sueelly@naver.com>
2025-02-17 20:56:28 +0900

    feat(AcceptanceTest): Add page params

--- src/test/java/ac/dnd/dodal/acceptance/plan_history/steps/GetPlansOfHistoryStep.java
+++ src/test/java/ac/dnd/dodal/acceptance/plan_history/steps/GetPlansOfHistoryStep.java
@@ -13,13 +13,15 @@ public class GetPlansOfHistoryStep {
     private static final String BASE_URL = "/api/goals/{goalId}/plan-histories/{historyId}";
 
     public static Response getPlansOfHistory(
-            Map<String, Object> header, Long goalId, Long historyId) {
+            Map<String, Object> header, Long goalId, Long historyId, int page, int size) {
         String url = BASE_URL.replace("{goalId}", goalId.toString())
                 .replace("{historyId}", historyId.toString());
 
         return given().log().all()
                 .contentType(ContentType.JSON)
                 .headers(header)
+                .queryParam("page", page)
+                .queryParam("size", size)
             .when()
                 .get(url)
             .then().log().all()
