30ad48a98ba7c8285d42855580ecb3f5af4fe3e6
5hseok <ohhs1010@gmail.com>
2025-02-17 19:48:55 +0900

    :recycle: refactor(OnBoardingResponseDto): Question과 Answer에 id도 함께 제공

--- src/main/java/ac/dnd/dodal/ui/onboarding/response/GetOnBoardingResponseDto.java
+++ src/main/java/ac/dnd/dodal/ui/onboarding/response/GetOnBoardingResponseDto.java
@@ -1,23 +1,23 @@
 package ac.dnd.dodal.ui.onboarding.response;
 
-import ac.dnd.dodal.domain.onboarding.enums.AnswerContent;
 import ac.dnd.dodal.domain.onboarding.model.Answer;
 import ac.dnd.dodal.domain.onboarding.model.Question;
-
 import java.util.List;
 
 public record GetOnBoardingResponseDto(
-        String question,
+        Long questionId,
+        String questionContent,
         String subContent,
         int order,
-        List<String> answers
+        List<AnswerResponseDto> answers
 ) {
+
     public static GetOnBoardingResponseDto of(Question question, List<Answer> answers) {
         return new GetOnBoardingResponseDto(
+                question.getId(),
                 question.getQuestionContent().getMainContent(),
                 question.getQuestionContent().getSubContent(),
                 question.getOrder(),
-                answers.stream().map(Answer::getContent).map(AnswerContent::getContent).toList()
-        );
+                answers.stream().map(AnswerResponseDto::of).toList());
     }
 }
