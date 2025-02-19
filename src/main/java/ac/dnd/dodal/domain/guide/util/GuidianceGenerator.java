d7e0d974128341854963e1caff831e1fa8f8627b
김혜주 <sueelly@naver.com>
2025-02-20 05:30:52 +0900

    rename(GuidianceGenerator): From GuidianceService

---
+++ src/main/java/ac/dnd/dodal/domain/guide/util/GuidianceGenerator.java
@@ -0,0 +1,15 @@
+package ac.dnd.dodal.domain.guide.util;
+
+import java.util.List;
+
+import ac.dnd.dodal.domain.guide.enums.NewGoalGuide;
+import ac.dnd.dodal.domain.guide.enums.NewPlanGuide;
+import ac.dnd.dodal.domain.guide.enums.UpdatePlanGuide;
+import ac.dnd.dodal.domain.guide.enums.UserType;
+import ac.dnd.dodal.domain.onboarding.enums.AnswerContent;
+import ac.dnd.dodal.domain.onboarding.exception.OnBoardingNotFoundException;
+import ac.dnd.dodal.domain.user.model.UserAnswer;
+
+public class GuidianceGenerator {
+
+}
