86a197dd971d1912438bff5e6940c7cc4dac8025
김혜주 <sueelly@naver.com>
2025-02-20 08:37:31 +0900

    refactor(PlanFeedbackFixture): Set question and indicator with actual value

--- src/test/java/ac/dnd/dodal/ui/feedback/fixture/FeedbackUIFixture.java
+++ src/test/java/ac/dnd/dodal/ui/feedback/fixture/FeedbackUIFixture.java
@@ -5,6 +5,6 @@ import ac.dnd.dodal.ui.feedback.request.CreateFeedbackRequest;
 public class FeedbackUIFixture {
 
     public static CreateFeedbackRequest createFeedbackRequest() {
-        return new CreateFeedbackRequest("question", "indicator");
+        return new CreateFeedbackRequest("조금 더 수월하게 달성하기 위해 뭘 해야할까요?", "우선순위를 재조정해요.");
     }
 }
