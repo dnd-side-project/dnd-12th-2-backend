2823f13756598268c0d409a37016afea562cde39
김혜주 <sueelly@naver.com>
2025-02-20 05:46:02 +0900

    feat(GuidanceGenerator): Re-throw illegalArgumentException to FAIL_TO_GENERATE

--- src/main/java/ac/dnd/dodal/domain/guide/util/GuidianceGenerator.java
+++ src/main/java/ac/dnd/dodal/domain/guide/util/GuidianceGenerator.java
@@ -2,10 +2,12 @@ package ac.dnd.dodal.domain.guide.util;
 
 import java.util.List;
 
+import ac.dnd.dodal.common.exception.InternalServerErrorException;
 import ac.dnd.dodal.domain.guide.enums.NewGoalGuide;
 import ac.dnd.dodal.domain.guide.enums.NewPlanGuide;
 import ac.dnd.dodal.domain.guide.enums.UpdatePlanGuide;
 import ac.dnd.dodal.domain.guide.enums.UserType;
+import ac.dnd.dodal.domain.guide.exception.UserGuideExceptionCode;
 import ac.dnd.dodal.domain.onboarding.enums.AnswerContent;
 import ac.dnd.dodal.domain.onboarding.exception.OnBoardingNotFoundException;
 import ac.dnd.dodal.domain.user.model.UserAnswer;
@@ -15,24 +17,39 @@ public class GuidianceGenerator {
     public static String generateNewGoalGuide(List<UserAnswer> userAnswers) {
         AnswerContent interestGoal = getInterestGoal(userAnswers);
 
-        NewGoalGuide newGoalGuide = NewGoalGuide.getByInterestGoal(interestGoal);
-        return newGoalGuide.getGuide();
+        try {
+            NewGoalGuide newGoalGuide = NewGoalGuide.getByInterestGoal(interestGoal);
+            return newGoalGuide.getGuide();
+        } catch (IllegalArgumentException e) {
+            throw new InternalServerErrorException
+                (UserGuideExceptionCode.FAIL_TO_GENERATE_USER_GUIDE, e);
+        }
     }
 
     public static String generateNewPlanGuide(List<UserAnswer> userAnswers) {
         AnswerContent preferenceSetPlan = getPreferenceSetPlan(userAnswers);
         AnswerContent difficultySetPlan = getDifficultySetPlan(userAnswers);
 
-        NewPlanGuide newPlanGuide = NewPlanGuide.getByPreferenceAndDifficultySetPlan(
-            preferenceSetPlan, difficultySetPlan);
-        return newPlanGuide.getGuide();
+        try {
+            NewPlanGuide newPlanGuide = NewPlanGuide.getByPreferenceAndDifficultySetPlan(
+                preferenceSetPlan, difficultySetPlan);
+            return newPlanGuide.getGuide();
+        } catch (IllegalArgumentException e) {
+            throw new InternalServerErrorException(
+                UserGuideExceptionCode.FAIL_TO_GENERATE_USER_GUIDE, e);
+        }
     }
 
     public static String generateUpdatePlanGuide(UserType userType, String feedbackIndicator) {
-        UpdatePlanGuide updatePlanGuide = UpdatePlanGuide.getByUserTypeAndFeedbackIndicator(
+        try {
+            UpdatePlanGuide updatePlanGuide = UpdatePlanGuide.getByUserTypeAndFeedbackIndicator(
                 userType.name(), feedbackIndicator);
 
-        return updatePlanGuide.getGuide();
+                return updatePlanGuide.getGuide();
+        } catch (IllegalArgumentException e) {
+            throw new InternalServerErrorException(
+                UserGuideExceptionCode.FAIL_TO_GENERATE_USER_GUIDE, e);
+        }
     }
 
     private static AnswerContent getInterestGoal(List<UserAnswer> userAnswers) {
