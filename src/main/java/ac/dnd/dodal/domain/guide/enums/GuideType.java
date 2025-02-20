cc347768dae6219e8072a45ffa4991148f5dd21f
김혜주 <sueelly@naver.com>
2025-02-20 10:16:07 +0900

    feat(GuideType): Add value using for passing params to url

--- src/main/java/ac/dnd/dodal/domain/guide/enums/GuideType.java
+++ src/main/java/ac/dnd/dodal/domain/guide/enums/GuideType.java
@@ -1,5 +1,7 @@
 package ac.dnd.dodal.domain.guide.enums;
 
+import java.util.Arrays;
+
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -7,11 +9,19 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum GuideType {
 
-    NEW_PLAN(NewPlanGuide.class),
-    NEW_GOAL(NewGoalGuide.class),
-    UPDATE_PLAN(UpdatePlanGuide.class),
-    USER_TYPE(UserType.class),
+    NEW_PLAN("new-plan", NewPlanGuide.class),
+    NEW_GOAL("new-goal", NewGoalGuide.class),
+    UPDATE_PLAN("update-plan", UpdatePlanGuide.class),
+    USER_TYPE("user-type", UserType.class),
     ;
 
+    private final String value;
     private final Class<? extends Enum<?>> guideClass;
+
+    public static GuideType of(String value) {
+        return Arrays.stream(GuideType.values())
+            .filter(type -> type.getValue().equals(value))
+            .findFirst()
+            .orElseThrow(() -> new IllegalArgumentException("Invalid guide type: " + value));
+    }
 }
