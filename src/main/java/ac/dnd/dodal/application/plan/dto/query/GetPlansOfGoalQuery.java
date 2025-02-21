cb7f03fc76df71f18fa201e894b48287c6e213d9
김혜주 <sueelly@naver.com>
2025-02-21 22:31:14 +0900

    feat(GetPlansOfGoalQuery): Add validation

--- src/main/java/ac/dnd/dodal/application/plan/dto/query/GetPlansOfGoalQuery.java
+++ src/main/java/ac/dnd/dodal/application/plan/dto/query/GetPlansOfGoalQuery.java
@@ -9,4 +9,19 @@ public record GetPlansOfGoalQuery(
     int range
 ) {
     
+    public GetPlansOfGoalQuery {
+        if (userId == null) {
+            throw new IllegalArgumentException("GetPlansOfGoalQuery: User ID is required");
+        }
+        if (goalId == null) {
+            throw new IllegalArgumentException("GetPlansOfGoalQuery: Goal ID is required");
+        }
+        if (date == null) {
+            throw new IllegalArgumentException("GetPlansOfGoalQuery: Date is required");
+        }
+        if (range < 0) {
+            throw new IllegalArgumentException(
+                "GetPlansOfGoalQuery: Range must be greater than 0");
+        }
+    }
 }
