4a421c603cff1269eb5b4098ab45e6fcbe4c21a4
김혜주 <sueelly@naver.com>
2025-02-21 15:27:34 +0900

    feat(GetPlansOfDateByGoal): Get plans with grouping by history id

--- src/main/java/ac/dnd/dodal/application/plan/service/PlanQueryService.java
+++ src/main/java/ac/dnd/dodal/application/plan/service/PlanQueryService.java
@@ -4,23 +4,25 @@ import java.util.List;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 
+import ac.dnd.dodal.common.exception.ForbiddenException;
 import ac.dnd.dodal.application.plan.repository.PlanRepository;
 import ac.dnd.dodal.application.plan.usecase.GetPlansOfHistoryUseCase;
 import ac.dnd.dodal.application.plan.usecase.GetPlansOfGoalByDateUseCase;
 import ac.dnd.dodal.application.plan.usecase.GetWeeklyAchievementRateOfGoalUseCase;
-import ac.dnd.dodal.common.exception.ForbiddenException;
 import ac.dnd.dodal.application.plan.dto.query.GetPlansOfHistoryQuery;
 import ac.dnd.dodal.application.plan.dto.query.GetPlansOfGoalQuery;
 import ac.dnd.dodal.application.plan.dto.query.GetWeeklyAchievementRateOfGoalQuery;
+import ac.dnd.dodal.application.plan.dto.PlanModel;
 import ac.dnd.dodal.ui.plan.response.PlanElement;
 import ac.dnd.dodal.ui.goal.response.DailyAchievementRateElement;
-// import ac.dnd.dodal.ui.plan.response.PlanWithHistoryElement;
+import ac.dnd.dodal.ui.plan.response.PlanWithHistoryElement;
 
 @Service
 @RequiredArgsConstructor
@@ -41,24 +43,28 @@ public class PlanQueryService implements
         return planRepository.findAllByHistoryId(query.historyId(), query.pageable());
     }
 
+    // 3일치의 completed 된 history 함께 제공
     @Override
-    public List<PlanElement> getPlansOfGoalByDate(GetPlansOfGoalQuery query) {
+    public List<PlanWithHistoryElement> getPlansOfGoalByDate(GetPlansOfGoalQuery query) {
         if (!planService.isExistByUserIdAndGoalId(query.userId(), query.goalId())) {
             throw new ForbiddenException();
         }
 
-        return planRepository.findAllByGoalIdAndDate(
-            query.goalId(), query.date().atStartOfDay(), query.date().atStartOfDay().plusDays(1));
+        List<PlanModel> plans = planRepository.findAllByGoalIdAndDate(query.goalId(),
+                query.date().atStartOfDay().minusDays(3), query.date().atStartOfDay().plusDays(1));
+        List<Long> historyIds =
+                plans.stream().map(PlanModel::getHistoryId).distinct().collect(Collectors.toList());
+        List<PlanWithHistoryElement> planWithHistoryElements = new ArrayList<>();
+        for (Long historyId : historyIds) {
+            List<PlanElement> plansByHistoryId =
+                    plans.stream().filter(plan -> plan.getHistoryId().equals(historyId)
+                        && plan.getCompletedDate() != null)
+                            .map(PlanModel::toPlanElement)
+                            .collect(Collectors.toList());
+            planWithHistoryElements.add(new PlanWithHistoryElement(historyId, plansByHistoryId));
+        }
+        return planWithHistoryElements;
     }
-    // @Override
-    // public List<PlanWithHistoryElement> getPlansOfGoalByDate(GetPlansOfGoalQuery query) {
-    //     if (!isExistByUserIdAndGoalId(query.userId(), query.goalId())) {
-    //         throw new ForbiddenException();
-    //     }
-
-    //     return planRepository.findAllWithPreviousHistoryByGoalIdAndDate(
-    //         query.goalId(), query.date().atStartOfDay());
-    // }
 
     // Todo: 로직 개선 필요. 너무 비효율적이야.
     @Override
@@ -71,7 +77,7 @@ public class PlanQueryService implements
         LocalDate startDate = query.date();
         LocalDate endDate = startDate.plusDays(7);
 
-        List<PlanElement> plans = planRepository.findAllByGoalIdAndDate(query.goalId(),
+        List<PlanModel> plans = planRepository.findAllByGoalIdAndDate(query.goalId(),
                 startDate.atStartOfDay(), endDate.atStartOfDay());
         List<DailyAchievementRateElement> dailyAchievementRateElements = new ArrayList<>();
 
@@ -79,7 +85,7 @@ public class PlanQueryService implements
             LocalDate date = startDate.plusDays(i);
             int successCount = 0;
             int totalCount = 0;
-            for (PlanElement plan : plans) {
+            for (PlanModel plan : plans) {
                 if (isPlanForDate(plan, date)) {
                     successCount++;
                 }
@@ -91,13 +97,13 @@ public class PlanQueryService implements
         return dailyAchievementRateElements;
     }
 
-    private boolean isPlanForDate(PlanElement plan, LocalDate date) {
+    private boolean isPlanForDate(PlanModel plan, LocalDate date) {
         LocalDateTime startDateTime = date.atStartOfDay();
         LocalDateTime endDateTime = date.atStartOfDay().plusDays(1);
 
-        return (plan.startDate().isBefore(endDateTime) &&
-                plan.endDate().isAfter(startDateTime)) ||
-                (plan.startDate().isBefore(endDateTime) &&
-                plan.endDate().isEqual(startDateTime));
+        return (plan.getStartDate().isBefore(endDateTime) &&
+                plan.getEndDate().isAfter(startDateTime)) ||
+                (plan.getStartDate().isBefore(endDateTime) &&
+                plan.getEndDate().isEqual(startDateTime));
     }
 }
