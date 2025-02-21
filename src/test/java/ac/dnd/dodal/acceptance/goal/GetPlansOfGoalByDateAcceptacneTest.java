37cb0ef880a905b0143029336d4f699dfcdac58c
김혜주 <sueelly@naver.com>
2025-02-21 22:24:34 +0900

    feat(getPlansOfGoal): Implement acceptance test

--- src/test/java/ac/dnd/dodal/acceptance/goal/GetPlansOfGoalByDateAcceptacneTest.java
+++ src/test/java/ac/dnd/dodal/acceptance/goal/GetPlansOfGoalByDateAcceptacneTest.java
@@ -24,9 +24,30 @@ public class GetPlansOfGoalByDateAcceptacneTest extends AcceptanceTest {
     @DisplayName("Get Plans of Goal by Date Test")
     public void get_plans_of_goal_by_date() {
         // when
-        Response response = GetPlansOfGoalByDateSteps
-                .getPlansOfGoalByDate(authorizationHeader, goalId, date);
+        Response response =
+                GetPlansOfGoalByDateSteps.getPlansOfGoalByDate(authorizationHeader, goalId, date, 1);
         ApiResponse<List<PlanWithHistoryElement>> apiResponse =
+                response.as(new TypeRef<ApiResponse<List<PlanWithHistoryElement>>>() {});
+
+        log.info("response: {}", response.asString());
+        log.info("apiResponse: {}", apiResponse);
+        // then 200
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        // COM001
+        assertThat(apiResponse.code()).isEqualTo(CommonResultCode.SUCCESS.getCode());
+        // Success
+        assertThat(apiResponse.message()).isEqualTo(CommonResultCode.SUCCESS.getMessage());
+        // data does not exist
+        assertThat(apiResponse.data().size()).isGreaterThan(0);
+    }
+
+    @Test
+    @DisplayName("Get Weekly Plans of Goal by Date Test")
+    public void get_weekly_plans_of_goal_by_date() {
+        // when
+        Response response = GetPlansOfGoalByDateSteps
+                .getPlansOfGoalByDate(authorizationHeader, goalId, date, 7);
+        ApiResponse<List<PlanWithHistoryElement>> apiResponse = 
             response.as(new TypeRef<ApiResponse<List<PlanWithHistoryElement>>>() {});
 
         log.info("response: {}", response.asString());
