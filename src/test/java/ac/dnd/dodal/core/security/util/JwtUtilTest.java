bd6a1ec1649a3d800011ef0c9678f926f465ad27
김혜주 <sueelly@naver.com>
2025-03-01 18:44:49 +0900

    refactor(JwtUtilTest): Add validation of result by extracting claims

--- src/test/java/ac/dnd/dodal/core/security/util/JwtUtilTest.java
+++ src/test/java/ac/dnd/dodal/core/security/util/JwtUtilTest.java
@@ -5,8 +5,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import io.jsonwebtoken.Claims;
+
 import ac.dnd.dodal.IntegrationTest;
 import ac.dnd.dodal.domain.user.enums.UserRole;
+import ac.dnd.dodal.common.constant.Constants;
 
 public class JwtUtilTest extends IntegrationTest {
 
@@ -17,15 +20,19 @@ public class JwtUtilTest extends IntegrationTest {
     String deviceToken = "device_token1";
     String nickname = "test1";
 
-    long millisecondsIn999Years = 999 * 365 * 24 * 60 * 60 * 1000; // 31,536,000,000,000
+    long millisecondsIn999Years = 9 * 365 * 24 * 60 * 60 * 1000L; // 31,536,000,000,000
     Long expirationPeriod = millisecondsIn999Years;
 
     @Test
     @DisplayName("Create JWT Token Success")
     public void create_jwt_token() {
+        // when
         String token = jwtUtil.createToken(userId, userRole, expirationPeriod);
-        System.out.println("Create JWT Token (999Years) = " + token);
 
-        assertThat(userId).isEqualTo(jwtUtil.getUserIdFromToken(token));
+        // then
+        Claims claims = jwtUtil.validateAndGetClaimsFromToken(token);
+        assertThat(userId).isEqualTo(claims.get(Constants.USER_ID_CLAIM_NAME, Long.class));
+        assertThat(userRole.name()).isEqualTo(
+            claims.get(Constants.USER_ROLE_CLAIM_NAME, String.class));
     }
 }
