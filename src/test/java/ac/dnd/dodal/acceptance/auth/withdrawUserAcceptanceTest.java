dc828dd126ef75c0b57bb0e32f3f19e8def76c97
5hseok <ohhs1010@gmail.com>
2025-03-13 04:25:58 +0900

    :white_check_mark: test(withdraw user): 유저 탈퇴 테스트 추가

---
+++ src/test/java/ac/dnd/dodal/acceptance/auth/withdrawUserAcceptanceTest.java
@@ -0,0 +1,33 @@
+package ac.dnd.dodal.acceptance.auth;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import ac.dnd.dodal.AcceptanceTest;
+import ac.dnd.dodal.acceptance.auth.steps.AuthSteps;
+import ac.dnd.dodal.common.enums.CommonResultCode;
+import ac.dnd.dodal.common.response.ApiResponse;
+import io.restassured.common.mapper.TypeRef;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.*;
+import org.springframework.http.HttpStatus;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class withdrawUserAcceptanceTest extends AcceptanceTest {
+
+    @Test
+    @Order(1000)
+    @DisplayName("Withdraw User Test")
+    public void withdrawUser() {
+        // when
+        Response response = AuthSteps.deleteUser(authorizationHeader);
+        ApiResponse<?> apiResponse =
+                response.as(new TypeRef<ApiResponse<?>>() {});
+        // then 200
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        // COM001
+        assertThat(apiResponse.code()).isEqualTo(CommonResultCode.SUCCESS.getCode());
+        // Success
+        assertThat(apiResponse.message()).isEqualTo(CommonResultCode.SUCCESS.getMessage());
+    }
+    
+}
