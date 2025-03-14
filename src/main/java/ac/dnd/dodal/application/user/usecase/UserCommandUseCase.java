674866036182cd3dbc7012059843e07fd6c72e25
5hseok <ohhs1010@gmail.com>
2025-03-14 18:07:17 +0900

    :fire: remove(auth): DeleteUserInfoResponseDto 삭제

--- src/main/java/ac/dnd/dodal/application/user/usecase/UserCommandUseCase.java
+++ src/main/java/ac/dnd/dodal/application/user/usecase/UserCommandUseCase.java
@@ -2,7 +2,6 @@ package ac.dnd.dodal.application.user.usecase;
 
 import ac.dnd.dodal.domain.user.model.User;
 import ac.dnd.dodal.ui.auth.request.OAuthUserInfoRequestDto;
-import ac.dnd.dodal.ui.auth.response.DeleteUserInfoResponseDto;
 
 public interface UserCommandUseCase {
     User createUserBySocialSignUp(OAuthUserInfoRequestDto authSignUpRequestDto);
