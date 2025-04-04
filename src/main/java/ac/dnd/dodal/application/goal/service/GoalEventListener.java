088fd52f3d20ab97a68aac4a7a0ce0e59abfc5ed
5hseok <ohhs1010@gmail.com>
2025-04-04 22:21:14 +0900

    :adhesive_bandage: fix(Goal): EventListener를 @Service가 아닌 @Component로 수정

--- src/main/java/ac/dnd/dodal/application/goal/service/GoalEventListener.java
+++ src/main/java/ac/dnd/dodal/application/goal/service/GoalEventListener.java
@@ -2,14 +2,12 @@ package ac.dnd.dodal.application.goal.service;
 
 import ac.dnd.dodal.application.goal.dto.command.DeleteAllGoalCommand;
 import ac.dnd.dodal.domain.user.event.UserWithdrawnEvent;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.event.EventListener;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
-@Service
+@Component
 @RequiredArgsConstructor
-@Transactional
 public class GoalEventListener {
 
     private final GoalCommandService goalCommandService;
